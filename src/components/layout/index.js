b882ba2f19d11c4d44037bb4797c70986ad2bac9
JSDev1999 <rameshlearningadvance@gmail.com>
2023-01-27 22:15:27 +0530

    updated

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,8 +1,14 @@
-import React, { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import React, { Fragment, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import Header from "./Header";
 
 export default function MainLayout() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      navigate("/app");
+    }
+  }, [navigate]);
   return (
     <Fragment>
       <Header />
