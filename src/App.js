4954d62c8998d1dbcd2c8958ffbdb868d24d05ab
JSDev1999 <rameshlearningadvance@gmail.com>
2023-01-31 00:12:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import React, { Fragment } from "react";
 import { useSelector } from "react-redux";
 import OptionItem from "./components/OptionItem";
 import Translation from "./components/Translation";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
   const { selectedOption } = useSelector((state) => state.user);
 
   return (
     <Fragment>
-      <Container component={"main"} maxWidth="md">
-        <OptionItem />
-        {Object.values(selectedOption).length !== 0 && <Translation />}
-      </Container>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+
+        <Container component={"main"} maxWidth="md">
+          <OptionItem />
+          {Object.values(selectedOption).length !== 0 && <Translation />}
+        </Container>
+      </ThemeProvider>
     </Fragment>
   );
 };
